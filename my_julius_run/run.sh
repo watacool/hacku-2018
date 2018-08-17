d3882e5acb84bfec9a6867cbe16947b937caa65b
watacool <cooffeemilk@gmail.com>
2018-08-17 16:24:35 +0900

    update for automatic activation

--- my_julius_run/run.sh
+++ my_julius_run/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MY_DICT=~/hacku-2018/dict/my_dict
-julius -C ~/julius-4.4.2/julius-kit/dictation-kit-v4.4/am-gmm.jconf -gram $MY_DICT -module > /dev/null &
-echo "Start julius in the background... PID = "$!
-sleep 1
-echo "Start recognitioning"
-python voice_recog.py
+export ALSADEV="plughw:1,0"
+/usr/local/bin/julius -C ~/julius-4.4.2/julius-kit/dictation-kit-v4.4/am-gmm.jconf -gram ~/hacku-2018/dict/my_dict -module > /dev/null &
+sleep 2
+cd ~/hacku-2018/my_julius_run; /usr/bin/nohup /usr/bin/python voice_recog.py &
